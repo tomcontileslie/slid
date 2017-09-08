adb4058327ed5a405af2280c906fd01ca5acbeab
Kieran <kieran.wallbanks@gmail.com>
2017-09-08 20:45:49 +0100

    Added the ability to edit cards, see #10

--- src/js/common.js
+++ src/js/common.js
@@ -28,3 +28,36 @@ function getParameterByName(name, url) {
   if (!results[2]) return "";
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
+
+/**
+ * Adds an option to a select dropdown
+ * @param {HTMLSelectElement} select the select element
+ * @param {string} key the text to be displayed in the option
+ * @param value the value when submitted or 'key' if omitted
+ */
+function addOptionToSelect(select, key, value) {
+  if (value == null) {
+    value = key;
+  }
+
+  select.options.add(new Option(key, value));
+}
+
+/**
+ * Returns a random v4 UUID of the form xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx, where each x is replaced with a random
+ * hexadecimal digit from 0 to f, and y is replaced with a random hexadecimal digit from 8 to b.
+ * @author https://gist.github.com/jed/982883
+ * @param a placeholder
+ * @returns {*} the uuid
+ */
+function uuidv4(a) {
+  return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,uuidv4)
+}
+
+/**
+ * Turns an undefined value to an empty string
+ * @param val the value
+ */
+function undefinedToEmpty(val) {
+  return val === undefined ? "" : val;
+}
